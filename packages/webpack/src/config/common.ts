8bfff741f1ee8c6689be3f52b9925bb82b0b4ce5
bpatath <github@bp.patathouse.fr>
2020-10-01 21:03:36 +0200

    Don't pre-compile graphql-tag on backend

--- packages/webpack/src/config/common.ts
+++ packages/webpack/src/config/common.ts
@@ -15,7 +15,7 @@ export const babelPlugins = [
   ["@babel/plugin-proposal-class-properties", { loose: true }],
   "@babel/plugin-proposal-object-rest-spread",
   "@babel/plugin-proposal-optional-chaining",
-  "graphql-tag",
+  ["graphql-tag", { importSources: ["@apollo/client"] }],
 ];
 if (isDev) {
   babelPlugins.push(require.resolve("react-refresh/babel"));

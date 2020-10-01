cf83dc073af27e8458850ad8f2f415ffeb7515ae
bpatath <github@bp.patathouse.fr>
2020-10-01 20:28:54 +0200

    Enable tree-shaking on to-be-packed libraries

--- .babelrc.js
+++ .babelrc.js
@@ -1,20 +1,35 @@
 module.exports = {
-	"presets": [
-      ["@babel/env", {
-        targets: {
-          node: true
-        }
-      }],
-      ["@babel/preset-react",{
-        development: process.env.NODE_ENV === "development",
-      }],
-			"@babel/typescript"
-	],
-	"plugins": [
-			"babel-plugin-transform-typescript-metadata",
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      ["@babel/plugin-proposal-class-properties", { "loose": true }],
-			"@babel/proposal-object-rest-spread",
-      ["babel-plugin-root-import", { "rootPathSuffix": "./src" }]
-	]
+  "env": {
+    "es5": {
+      "presets": [
+        ["@babel/env", {
+          targets: { node: true },
+        }],
+        ["@babel/preset-react",{
+          development: process.env.NODE_ENV === "development",
+        }],
+        "@babel/typescript"
+      ],
+    },
+    "modules": {
+      "presets": [
+          ["@babel/env", {
+            modules: false
+          }],
+          ["@babel/preset-react",{
+            development: process.env.NODE_ENV === "development",
+          }],
+          "@babel/typescript"
+      ]
+    }
+  },
+  "plugins": [
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-optional-chaining",
+    ["babel-plugin-root-import", { "rootPathSuffix": "./src" }],
+    "graphql-tag",
+  ]
 }
---
+++ Makefile
@@ -0,0 +1,16 @@
+BABEL_OPTS=--extensions .ts,.tsx --delete-dir-on-start --config-file ../../.babelrc.js 
+
+build-webpack:
+	cd packages/webpack && BABEL_ENV=es5 babel ${BABEL_OPTS} -d lib src
+
+build-backend:
+	cd packages/backend && BABEL_ENV=es5 babel ${BABEL_OPTS} -d lib/es5 src
+	cd packages/backend && BABEL_ENV=modules babel ${BABEL_OPTS} -d lib/modules src
+
+build-frontend:
+	cd packages/frontend && BABEL_ENV=modules babel ${BABEL_OPTS} -d lib src
+
+build-ui:
+	cd packages/ui && BABEL_ENV=modules babel ${BABEL_OPTS} -d lib src
+
+build: build-webpack build-backend build-frontend build-ui
